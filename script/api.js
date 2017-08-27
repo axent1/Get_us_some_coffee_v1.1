9d3c91570fc35f55cb67e5b4a36b8000ff00c50c
axent1 <31309373+axent1@users.noreply.github.com>
2017-08-26 19:23:08 -0700

    Test1
    test1

--- script/api.js
+++ script/api.js
@@ -1,52 +1,37 @@
-// There are lots of ways to search. See: https://developer.foursquare.com/docs/venues/search
-// Try stuff out with the API Exporer: https://developer.foursquare.com/docs/explore
-/*
-https://api.foursquare.com/v2/venues/search?near=NYU
-&oauth_token=3FFPGFHJDBSIFINX045
-&v=20140626
+    var client_id = 'OZZ0YQOCRCTTEEHRG3HL0IWMKTAJ0KCRPU1WS3O5WVINZI3K';
+    var client_secret = 'Z11HIZTIKBNALPZXJNFRPAF3XR0U3IJEYYHWIO4SJHY43SNG';
+    var base_url = 'https://api.foursquare.com/v2/';
+    var endpoint = 'venues/search?';
 
-*/
-var client_id = 'YOUR CLIENT ID HERE';
-var client_secret = 'YOUR CLIENT SECRET HERE';
-var base_url = 'https://api.foursquare.com/v2/';
-var endpoint = 'venues/search?';
+    var params = 'near=Nis, Serbia&query=coffee'; 
+    var key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20140626';
+    var url = base_url + endpoint + params + key;
+    var map;
+    var mesto;
 
-var params = 'near=New+York+University'; 
-var key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20140626';
-var url = base_url+endpoint+params+key;
-
-//set up map
-var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: new google.maps.LatLng(40.729,-73.996),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-$.get(url, function (result) {
-    //$('#msg pre').text(JSON.stringify(url));
-    $('#msg pre').text(JSON.stringify(result));
-    
-    var venues = result.response.venues;
-    //printVenues(venues); 
-    for (var i in venues){
-        var venue = venues[i];       
-        // place the a marker on the map
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(venue.location.lat,venue.location.lng),
-            map: map
-          });
-    }});
-
-
-function printVenues(venuesArr){  
-    for (var i in venuesArr){
-        var venue = venuesArr[i];
-        var str = '<p><strong>' + venue.name + '</strong> ';   
-        str += venue.location.lat + ',';
-        str += venue.location.lng;
-        str += '</p>';
-        $('#display').append(str);
+    //set up map
+    function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 15,
+                center: new google.maps.LatLng(43.321421, 21.895247),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
     }
-}
 
-
+    //generate markers
+    $.get(url, function (result) {
+        $('#msg pre').text(JSON.stringify(url));
+        //$('#msg pre').text(JSON.stringify(result));
+        var mesta = result.response.venues;
+        $('#display').append(mesta);
+        for (var i in mesta){
+            mesto = mesta[i];
+            var str = '<p><strong>' + mesto.name + '</strong> '; 
+            str += '</p>';
+            $('#display').append(str);
+            // place the a marker on the map
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(mesto.location.lat,mesto.location.lng),
+                map: map
+            });
+        }});
