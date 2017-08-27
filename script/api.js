7beee586bda4cb96963616b78e8f625f2b832bdc
axent1 <31309373+axent1@users.noreply.github.com>
2017-08-27 10:21:50 -0700

    Test4
    test4

--- script/api.js
+++ script/api.js
@@ -1,21 +1,13 @@
-// inicijalizacija API kljuceva
-var client_id = 'OZZ0YQOCRCTTEEHRG3HL0IWMKTAJ0KCRPU1WS3O5WVINZI3K';
-var client_secret = 'Z11HIZTIKBNALPZXJNFRPAF3XR0U3IJEYYHWIO4SJHY43SNG';
-
-// inicijalizacija API ulr adrese za pretragu 
-var base_url = 'https://api.foursquare.com/v2/';
-var endpoint = 'venues/explore?';
-var params = 'near=Nis, Serbia&query=coffee'; 
-var key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20140626';
-var url = base_url + endpoint + params + key;
-
-// inicijalizacija promenljivih
+// Deklaracija API promenljivih
+var client_id,client_secret,base_url,endpoint,url,params,key
+var limit,sortiranje,tip_pretrage,radijus,ll;
+var sort = 1;
+// Deklaracija ostalih promenljivih
 var str;
 var pozicija_korisnika;
 var mesta;
-var pozicija_markera;
-var udaljenost = 1000.0;
 var map;
+var options;
 var infoWindow;
 
 // Pozicija korisnika i iscrtavanje mape.
@@ -30,62 +22,99 @@ function initMap() {
     infoWindow = new google.maps.InfoWindow;
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
+        options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
         navigator.geolocation.getCurrentPosition(function(position) {
-
-        pozicija_korisnika = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-
+        pozicija_korisnika = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(pozicija_korisnika.lat,pozicija_korisnika.lng),
+            animation: google.maps.Animation.BOUNCE,
+            icon: '../images/user_marker.png',
+            title: 'Tvoja trenutna pozicija',
+            map: map
+        });
         infoWindow.setPosition(pozicija_korisnika);
-        infoWindow.setContent('Location found.');
+        infoWindow.setContent('Tvoja Lokacija');
         infoWindow.open(map);
         map.setCenter(pozicija_korisnika);
-        }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-        });
+        InitApiUrl();
+        }, 
+        function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        },
+        options
+    );
     } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
     }
 }
+
+// funkcija koja podnosi greske
 function handleLocationError(browserHasGeolocation, infoWindow, pozicija_korisnika) {
     infoWindow.setPosition(pozicija_korisnika);
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
-    }
+}
 
-// Generisanje markera
-$.get(url, function (result) {
-mesta = result.response.groups[0].items;  
+function InitApiUrl(){
+    // inicijalizacija API endpointa i kljuceva
+    client_id = 'OZZ0YQOCRCTTEEHRG3HL0IWMKTAJ0KCRPU1WS3O5WVINZI3K';
+    client_secret = 'Z11HIZTIKBNALPZXJNFRPAF3XR0U3IJEYYHWIO4SJHY43SNG';
+    base_url = 'https://api.foursquare.com/v2/';
+    endpoint = 'venues/explore?';
 
-for (var i in mesta){
-    mesta[i];
-    pozicija_markera = new google.maps.LatLng(mesta[i].venue.location.lat, mesta[i].venue.location.lng);
+    // inicijalizacija API pretrage
+    limit = 10;
 
-    if(calcDistance(pozicija_korisnika,pozicija_markera) <= udaljenost){
-        str = '<p><strong>' + mesta[i].venue.name + '</strong> '; 
-        str += '(jeste)';
-        if(mesta[i].venue.hours){
-            str += '-----'+ mesta[i].venue.hours.isOpen +'</p>';            
-        }
-        $('#display').append(str);
-        if(mesta[i].venue.hours && mesta[i].venue.hours.isOpen == true){
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(mesta[i].venue.location.lat,mesta[i].venue.location.lng),
-            map: map
-        });
-    }
+    if(sort == 1){
+        sortiranje = '&sortByDistance=1';
     }else{
-        str = '<p><strong>' + mesta[i].venue.name + '</strong> '; 
-        str += '(nije)';
-        if(mesta[i].venue.hours.isOpen){
-            str += '-----'+ mesta[i].venue.hours.isOpen +'</p>';            
-        }
-        $('#display').append(str);
+        sortiranje = '';
     }
-}});
 
+    tip_pretrage = 'coffee';
+    radijus = 1000.0;
+    ll = pozicija_korisnika.lat+', '+pozicija_korisnika.lng;
+
+    // inicijalizacija parametra za API, url adrese i kljuca; 
+    params = 'll=' + ll + '&query=' + tip_pretrage + '&radius=' + radijus + '&limit=' + limit + '&openNow=1' + sortiranje;
+    key = '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=' + '20140626';
+    url = base_url + endpoint + params + key;
+    GenerateVenues();
+}
+
+// Generisanje 10 markera u zadatom radijusu po udaljenosti.
+function GenerateVenues(){
+    $.get(url, function (result) {
+        mesta = result.response.groups[0].items;  
+        for (var i in mesta){
+            str = '<p><strong>' + mesta[i].venue.name + '</strong> '; // zameni sa javascript a ne Jquery (i vidi isto za url ako umes)
+            str += '(jeste)';
+            $('#display').append(str);
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(mesta[i].venue.location.lat,mesta[i].venue.location.lng),
+                animation: google.maps.Animation.DROP,
+                map: map
+            });
+        }
+    });
+}
+function SortirajPo(value)
+{   
+    sort = value;
+    document.getElementById("display").innerHTML = "";
+    initMap();
+}
 // Proverava da li je lokacija u datom radijusu.
 function calcDistance(p1, p2) {
     return (google.maps.geometry.spherical.computeDistanceBetween(p1,p2)).toFixed(2);
-  }
+}
